77b0de43c0f2a5499ad0e5e2e93ccfb8b8130f63
Greg Black <41589694+gblack686@users.noreply.github.com>
2019-12-11 00:22:49 -0500

    Chapter1

---
+++ Chapter_1/Exercise2/function.go
@@ -0,0 +1,17 @@
+// package main 
+
+// import (
+// 	"fmt" 
+// 	"net/http"
+// )
+// func WelcomeServerless(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello Serverless World")
+// }
+
+
+package main
+import (
+	"fmt"
+	"net/http" )
+	
+func WelcomeServerless(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "Hello Serverless World!") }
